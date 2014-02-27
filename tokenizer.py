3f28c229a35e40350af85726a4514d9adf6c4406
c3h3 <c3h3.tw@gmail.com>
2014-02-27 13:00:42 +0800

    create tokenizer module

---
+++ tokenizer.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 27, 2014
+
+@author: c3h3
+'''
+
+ngram_no_filter = lambda text, n: (text[k:k+n] for k in range(len(text)-n+1))
+
+ngram = lambda text, n, filter_list=[]: (text[k:k+n] for k in range(len(text)-n+1) if not(text[k:k+n] in filter_list))
+
+
+if __name__ == '__main__':
+    pass
