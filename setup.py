00f83f05cf24125fcbdd69dd3d47a2fce281258a
c3h3 <c3h3.tw@gmail.com>
2014-02-27 09:45:36 +0800

    rename

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='simplenlp',
+    name='PlaYnlp',
     version='0.0.1',
     description='Simple NLP Tools',
     author='Chia Chi Chang & Willy Kuo',
     author_email='c3h3.tw@gmail.com & waitingkuo0527@gmail.com',
-    packages=['simplenlp'],
+    packages=['PlaYnlp'],
     install_requires=[
         'scikit-learn',
         'numpy',
