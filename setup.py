13452c70fbc24e64f78eecafa332692e2a5d46de
c3h3 <c3h3.tw@gmail.com>
2014-03-12 16:45:14 +0800

    change the structure of package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='PlaYnlp',
     version='0.0.1dev',
     description='PlaY NLP Toolkits',
-    author='Chia Chi Chang & Willy Kuo',
+    author='Chia-Chi Chang & Willy Kuo',
     author_email='c3h3.tw@gmail.com & waitingkuo0527@gmail.com',
     packages=find_packages(),
     install_requires=[
